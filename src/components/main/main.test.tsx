3af546bc329d35ca5a7cc9d0a9bd5e0aaa6ae1b1
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-08-17 03:25:34 -0300

    refactor: checking colors and fixing heading text

--- src/components/main/main.test.tsx
+++ src/components/main/main.test.tsx
@@ -7,9 +7,15 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /Aug\/2021/i })
+      screen.getByRole('heading', { name: /Boilerplate Aug\/2021/i })
     ).toBeInTheDocument()
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
