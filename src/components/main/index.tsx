b650c8b77ea29782aa5382b8d06dfde97276ebc4
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-08-17 03:21:41 -0300

    feat: first styled component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,13 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Aug/2021</h1>
-  </main>
+  <S.Wrapper>
+    <S.Title>Boilerplate Aug/2021</S.Title>
+    <S.Description>
+      Created with Typescript, ReactJS, NextJS, Styled Components, Jest, Husky,
+      Eslint and Prettier
+    </S.Description>
+  </S.Wrapper>
 )
 
 export default Main
