ea108e55a651d6ac828adf800928f26206aa4cb0
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-08-17 04:33:11 -0300

    refactor: adding custom favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,15 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Boilerplate</title>
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="favicon.ico" />
+        <link
+          rel="shortcut icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta name="description" content="A simple project" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092b" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
